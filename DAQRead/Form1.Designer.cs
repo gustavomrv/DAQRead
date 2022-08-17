993d2330f23e71aa02cee087e42252f82ec05523
gustavomrv <gustavomourago@gmail.com>
2022-08-17 20:00:40 -0300

    up

--- DAQRead/Form1.Designer.cs
+++ DAQRead/Form1.Designer.cs
@@ -31,13 +31,22 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(188, 42);
+            this.button1.Location = new System.Drawing.Point(192, 43);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.Size = new System.Drawing.Size(89, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Ler";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,26 +54,119 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 42);
+            this.textBox1.Location = new System.Drawing.Point(17, 43);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(123, 23);
+            this.textBox1.Size = new System.Drawing.Size(124, 23);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Location = new System.Drawing.Point(17, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.Size = new System.Drawing.Size(84, 15);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Leitura Analógica";
+            this.label1.Text = "Tensão Elétrica";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Location = new System.Drawing.Point(147, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "V";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Corrente Elétrica";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(17, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Location = new System.Drawing.Point(147, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "A";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Ler";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Potência Elétrica";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(17, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 23);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label6.Location = new System.Drawing.Point(146, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "W";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Ler";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 95);
+            this.ClientSize = new System.Drawing.Size(287, 223);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -82,5 +184,14 @@
         private Button button1;
         private TextBox textBox1;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Button button2;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private Button button3;
     }
 }
--- DAQRead/Form1.cs
+++ DAQRead/Form1.cs
@@ -11,22 +11,21 @@ namespace DAQRead
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        {}
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        {}
+        private void label2_Click(object sender, EventArgs e)
+        {}
+        private void label3_Click(object sender, EventArgs e)
+        {}
+        private void label4_Click(object sender, EventArgs e)
+        {}
         private void button1_Click(object sender, EventArgs e)
         {
             Task analogInTask = new Task();
 
             AIChannel myAIChannel;
-            AIChannel myAIChannel2;
 
             myAIChannel = analogInTask.AIChannels.CreateVoltageChannel(
                 "dev1/ai0",
@@ -37,33 +36,6 @@ namespace DAQRead
                 AIVoltageUnits.Volts
                 );
 
-            myAIChannel2 = analogInTask.AIChannels.CreateCurrentChannel(
-                "dev/ai0",
-                "myAIChannel",
-                AITerminalConfiguration.Differential,
-                0,
-                20,
-                AICurrentUnits.Amps
-                );
-
-            myAIChannel2 = analogInTask.AIChannels.CreateCurrentChannel(
-                "dev/ai0",
-                "myAIChannel",
-                AITerminalConfiguration.Differential,
-                0,
-                20,
-                AICurrentUnits.Amps
-                );
-
-            //myAIChannel2 = analogInTask.AIChannels.CreateResistanceChannel(
-            //    "dev/ai0",
-            //    "myAIChannel",
-            //    AITerminalConfiguration.Differential,
-            //    0,
-            //    20,
-            //    AIResistanceUnits.Ohms,
-            //    );
-
             AnalogSingleChannelReader reader = new AnalogSingleChannelReader(analogInTask.Stream);
 
             double analogDataIn = reader.ReadSingleSample();
@@ -71,5 +43,26 @@ namespace DAQRead
             textBox1.Text = analogDataIn.ToString("0.00");
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task analogInTask2 = new Task();
+
+            AIChannel myAIChannel2;
+
+            myAIChannel2 = analogInTask2.AIChannels.CreateCurrentChannel(
+                "dev/ai0",
+                "myAIChannel",
+                AITerminalConfiguration.Differential,
+                0,
+                20,
+                AICurrentUnits.Amps
+                );
+
+            AnalogSingleChannelReader reader2 = new AnalogSingleChannelReader(analogInTask2.Stream);
+
+            double analogDataIn2 = reader2.ReadSingleSample();
+
+            textBox2.Text = analogDataIn2.ToString("0.00");
+        }
     }
 }
