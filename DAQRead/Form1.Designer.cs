8ee0119a06cb0d5eab05968e12308eb23fd96a2f
gustavomrv <gustavomourago@gmail.com>
2022-08-17 18:02:35 -0300

    up

--- DAQRead/Form1.Designer.cs
+++ DAQRead/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ler";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Leitura Analógica";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(293, 95);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Leitura de Dados DAQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
     }
 }
--- DAQRead/Form1.cs
+++ DAQRead/Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Windows.Forms;
+using NationalInstruments.DAQmx;
+using Task = NationalInstruments.DAQmx.Task;
+
 namespace DAQRead
 {
     public partial class Form1 : Form
@@ -6,5 +11,37 @@ namespace DAQRead
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task analogInTask = new Task();
+
+            AIChannel myAIChannel;
+
+            myAIChannel = analogInTask.AIChannels.CreateVoltageChannel(
+                "dev1/ai0",
+                "myAIChannel",
+                AITerminalConfiguration.Differential,
+                0,
+                10,
+                AIVoltageUnits.Volts
+                );
+
+            AnalogSingleChannelReader reader = new AnalogSingleChannelReader(analogInTask.Stream);
+
+            double analogDataIn = reader.ReadSingleSample();
+
+            textBox1.Text = analogDataIn.ToString("0.00");
+        }
+
     }
 }
